d32a6656b8a61049bd13f8759f5b2c834df50807
lemuayala <lemuayala@gmail.com>
2025-04-18 16:27:26 -0300

    Se eliminan commands and Queries para operaciones CRUD

--- src/Features/Products/Application/Commands/CreateProductCommand.cs
+++
@@ -1,9 +0,0 @@
-using MediatR;
-
-namespace EcomerceAI.Api.Features.Products.Application.Commands;
-
-public record CreateProductCommand(
-    string Name,
-    string Description,
-    decimal Price,
-    string Category) : IRequest<int>;
--- src/Features/Products/Application/Queries/GetProductByIdQuery.cs
+++
@@ -1,6 +0,0 @@
-using MediatR;
-
-namespace EcomerceAI.Api.Features.Products.Application.Queries
-{
-    public record GetProductByIdQuery(int Id) : IRequest<Product>;
-}
